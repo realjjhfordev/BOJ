ddc41bacc0401b4af80121434b757a7dd9646411
Jaeho <realjjhfordev@gmail.com>
2023-07-27 23:37:41 +0900

    92

---
+++ barkingdog/0x0b-recursive/recusive_example.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void func1(int n)
+{
+  if (n == 0) return 0;
+  cout << 'n' << ' ';
+  func1(n-1);
+}
+//n부터 1까지 출력하는 함수
+
+void func2(int n)
+{
+  if (n == 0) return 0;
+  return n+func2(n-1);
+}
+// n부터 1까지의 합을 출력하는 함수
